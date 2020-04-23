0f666a2682da067b3cfacfc342e8bca76c9a2c5f
hoshomoh <oshomo.oforomeh2010@gmail.com>
2020-04-23 15:02:26 +0200

    - Added BDD feature and snippet generator command

---
+++ bin/codecept-cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const commander = require('commander');
+const codeceptHandler = require('../lib/commands/codecept');
+
+commander.usage('[command] [options]');
+commander.version('1.0.0');
+
+commander.command('bdd:feature <name>')
+    .description('Generate feature and step file for a feature.')
+    .action(require('../lib/commands/generator/feature'));
+
+commander.command('bdd:snippets <name>')
+    .description('Generate snippets for a feature.')
+    .action(require('../lib/commands/generator/snippet'));
+
+commander.on('command:*',  (cmd) => {
+  codeceptHandler(cmd);
+});
+
+if (process.argv.length <= 2) {
+  commander.outputHelp();
+}
+
+commander.parse(process.argv);
